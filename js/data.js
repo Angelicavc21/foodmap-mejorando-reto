ea09b35292d1ad63735a44cbf3e79428d649639a
Angelicavc21 <angelik_m21@hotmail.com>
2018-01-20 09:30:36 -0500

    mejorando reto pasado

---
+++ js/data.js
@@ -0,0 +1,104 @@
+var data = [{
+
+
+  'name': 'Polleria Don Juan',
+  'type': 'chicken',
+  'stars': 3,
+  'description': 'Av. Larco 1268',
+  'money': '$',
+  'image': '../assets/images/pollo1.png'
+
+},
+{
+  'name': 'Manduco Café Ristorante Italiano',
+  'type': 'italiana',
+  'stars': 5,
+  'description': 'Barranco, Av. Almte. Miguel Grau 330, Lima',
+  'money': '$$$',
+  'image': '../assets/images/italiana1.jpg'
+
+},
+{
+  'name': 'El Rey Marino',
+  'type': 'fish',
+  'stars': 4,
+  'description': 'Calle Clara Bartón, Surquillo 15038',
+  'money': '$$$',
+  'image': '../assets/images/fish.jpg'
+
+},
+{
+  'name': 'punto azul',
+  'type': 'fish',
+  'stars': 3,
+  'description': 'Av Benavides 1450',
+  'money': '$$$',
+  'image': '../assets/images/fish2.jpg'
+
+},
+{
+  'name': 'Chifa Royal',
+  'type': 'china',
+  'stars': 3,
+  'description': 'Av. Guillermo Prescott 231, San Isidro 15076',
+  'money': '$$',
+  'image': '../assets/images/china1.jpg'
+},
+{
+  'name': 'barra mar',
+  'type': 'fish',
+  'stars': 5,
+  'address': 'Barranco',
+  'money': '$$$',
+  'image': '../assets/images/fish.jpg'
+},
+{
+  'name': 'La Linterna',
+  'type': 'italiana',
+  'stars': 4,
+  'description': ' Av. Caminos del Inca 459, Santiago de Surco 15038',
+  'money': '$$',
+  'image': '../assets/images/italiana2.png'
+},
+{
+  'name': 'EL REAL SABOR',
+  'type': 'chicken',
+  'stars': 4,
+  'description': 'Av Revolucion 1896, Villa EL Salvador 15834',
+  'money': '$$$',
+  'image': '../assets/images/pollo2.jpg'
+},
+{
+  'name': 'Panda Garden',
+  'type': 'china',
+  'stars': 3,
+  'description': 'Av. Benavides 4437, Surco, Lima',
+  'money': '$$$',
+  'image': '../assets/images/china2.jpg'
+},
+{
+  'name': 'El point',
+  'type': 'fish',
+  'stars': 5,
+  'description': 'Av. Velazco 52, VES',
+  'money': '$$$',
+  'image': '../assets/images/fish2.jpg'
+},
+{
+  'name': 'Pollos Y Parrillas Ala Leña',
+  'type': 'chicken',
+  'stars': 3,
+  'description': 'Av. La Marina 665, La Perla 07011',
+  'money': '$$',
+  'image': '../assets/images/pollo3.jpg'
+},
+{
+  'name': 'Don Rosalino',
+  'type': 'italiana',
+  'stars': 4,
+  'description': 'Av. República de Panamá 5897, Miraflores 15048',
+  'money': '$$',
+  'image': '../assets/images/italiana3.jpg'
+}
+
+];
